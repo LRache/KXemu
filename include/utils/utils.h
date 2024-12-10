5afad525a0588baad657e9bac6767aad270bcd7a
rache <2746324475@qq.com>
2024-12-10 23:40:28 +0800

    feat: utils function string_to_word

---
+++ include/utils/utils.h
@@ -0,0 +1,12 @@
+#ifndef __UTILS_UTILS_H__
+#define __UTILS_UTILS_H__
+
+#include "common.h"
+
+#include <string>
+
+namespace utils {
+    word_t string_to_word(const std::string &s);
+}
+
+#endif // __UTILS_UTILS_H__
---
+++ src/utils/utils/utils.cpp
@@ -0,0 +1,12 @@
+#include "utils/utils.h"
+#include "common.h"
+
+word_t utils::string_to_word(const std::string &s) {
+    word_t result;
+    if (s.size() > 2 && s[0] == '0' && s[1] == 'x') {
+        result = std::stoul(s, nullptr, 16);
+    } else {
+        result = std::stoul(s);
+    }
+    return result;
+}
