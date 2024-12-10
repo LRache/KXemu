9b628aaadf8da3fa507dfac012ae8fb399cfcdc4
Rache <2746324475@qq.com>
2024-12-10 19:10:24 +0800

    refactor: rename ./src/kdb/memory-map.cpp -> ./src/kdb/memory.cpp

---
+++ src/kdb/memory.cpp
@@ -0,0 +1,23 @@
+#include "kdb/config.h"
+#include "kdb/kdb.h"
+#include "memory/map.h"
+#include "memory/memory.h"
+#include "test_img.h"
+#include <cstring>
+
+#define TEST_IMG test_img_c_addi
+
+Memory *kdb::memory = nullptr;
+
+void kdb::init_memory() {
+    memory = new Memory();
+    StorageMemoryMap *mem = new StorageMemoryMap(MEM_SIZE);
+    memory->add_memory_map("mem", MEM_BASE, MEM_SIZE, mem);
+
+    std::memcpy(mem->data, TEST_IMG, sizeof(TEST_IMG));
+}
+
+void kdb::deinit_memory() {
+    memory->free_all();
+    delete memory;
+}
