9d50de53161746fe40c7da0e401d8edd62742d77
rache <2746324475@qq.com>
2024-12-11 16:29:51 +0800

    refactor: use args_t type instead of std::vector

--- include/kdb/kdb.h
+++ include/kdb/kdb.h
@@ -6,6 +6,7 @@
 #include "memory/memory.h"
 
 #include <string>
+#include <map>
 
 namespace kdb {
     void init();
@@ -18,9 +19,15 @@ namespace kdb {
     void run_cmd_mainloop();
     int run_command(const std::string &cmd);
     int run_cpu();
+    int step_core(Core *core);
+
+    int run_source_file(const std::string &filename);
 
     // Load elf to memory, return 0 if failed and entry else.
     word_t load_elf(const std::string &filename);
+    extern std::string elfFileName;
+    extern std::map<word_t, std::string> symbolTable;
+    extern word_t programEntry;
 
     extern Memory *memory;
     extern CPU *cpu;
