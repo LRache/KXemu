8f6fde06c5a826689f43d0da47f61e0285c2ad40
rache <2746324475@qq.com>
2024-12-15 00:17:17 +0800

    feat: add uart16550 device

---
+++ include/device/uart.h
@@ -0,0 +1,95 @@
+#ifndef __DEVICE_UART_H__
+#define __DEVICE_UART_H__
+
+#include "memory/map.h"
+#include <cstdint>
+#include <ostream>
+#include <queue>
+#include <sys/types.h>
+
+#define UART_LENGTH 8
+
+class Uart16650: public MemoryMap {
+public:
+    word_t read(word_t offset, int size);
+    bool write(word_t offset, word_t data, int size);
+    uint8_t *get_ptr(word_t offset);
+    std::string get_type_str() const;
+
+    bool input_data(uint8_t data);
+    void set_output_stream(std::ostream &os);    // send data to stream
+    void open_socket(const std::string &ip, int port); // open socket to send and receive data
+
+    enum Mode {
+        NONE,
+        STREAM,
+        SOCKET,
+    };
+    
+    ~Uart16650() = default;
+
+private:
+    int mode = Mode::NONE;
+    std::ostream *stream = nullptr;
+
+    std::queue<uint8_t> queue; // FIFO buffer
+    void send_byte(uint8_t c);
+
+    // Divisor Latch Register
+    // [0-7] RW - Divisor Latch
+    uint8_t lsb = 0x00; // Divisor Latch Low
+    uint8_t msb = 0x00; // Divisor Latch High
+
+    // Interrupt Enable Register
+    // [0] RW - Enable Received Data Available Interrupt
+    // [1] RW - Enable Transmitter Holding Register Empty Interrupt
+    // [2] RW - Enable Receiver Line Status Interrupt
+    // [3] RW - Enable Modem Status Interrupt
+    // [4-7] RW - Reserved
+    uint8_t ier = 0b00000000; // reset value
+
+    // Interrupt Identification Register
+    // [0-3] R - Interrupt Identification
+    // [4-5] R - 0
+    // [6-7] R - 1
+    uint8_t iir = 0b11000001; // reset value
+
+    // FIFO Control Register
+    // [0] W - Ignored
+    // [1] W - Clear Receive FIFO
+    // [2] W - Clear Transmit FIFO
+    // [3-5] W - Ignored
+    // [6-7] R - Receiver FIFO Trigger Level
+    uint8_t fcr = 0b11000000; // reset value
+
+    // Line Control Register
+    // [0-1] RW - Word Length
+    // [2] RW - Stop Bit
+    // [3] RW - Parity Enable
+    // [4] RW - Parity Type
+    // [5] RW - Strick Parity
+    // [6] RW - Break Control
+    // [7] RW - Divisor Latch Access Bit
+    uint8_t lcr = 0b00000011; // reset value
+
+    // Line Status Register
+    // [0] R - Data Ready
+    // [1] R - Overrun Error
+    // [2] R - Parity Error
+    // [3] R - Framing Error
+    // [4] R - Break Interrupt
+    // [5] R - Transmitter Holding Register Empty
+    // [6] R - Transmitter Empty
+    // [7] R - FIFO Data Error
+    uint8_t lsr;
+
+    // NOTE: The following registers are not implemented
+    // Modem Status Register
+    // [0] R - Delta Clear to Send
+    // [1] R - Delta Data Set Ready
+    // [2] R - Trailing Edge Ring Indicator
+    // [3] R - Delta Data Carrier Detect
+    uint8_t msr;
+};
+
+#endif
---
+++ src/device/uart.cpp
@@ -0,0 +1,135 @@
+#include "device/uart.h"
+#include "isa/word.h"
+#include "log.h"
+
+#include <cstdint>
+#include <iostream>
+
+#define BUFFER_SIZE 1024
+
+word_t Uart16650::read(word_t offset, int size) {
+    if (size != 1) {
+        WARN("uart read size %d not supported.", size);
+        return 0;
+    }
+    if (offset == 0) {
+        if (lcr & 0x80) { // Divisor Latch Access Bit is set
+            // LSB: Lower 8 bits of the divisor latch
+            return lsb;
+        } else {
+            // RBR: Receiver Buffer Register
+            if (queue.empty()) {
+                return 0;
+            }
+            char c = queue.front();
+            queue.pop();
+            return c;
+        }
+    } else if (offset == 1) {
+        if (lcr & 0x80) { // Divisor Latch Access Bit is set
+            // MSB: Upper 8 bits of the divisor latch
+            return msb;
+        } else {
+            // IER: Interrupt Enable Register
+            return ier;
+        }
+    } else if (offset == 2) {
+        // IIR: Interrupt Identification Register
+        return iir;
+    } else if (offset == 3) {
+        // LCR: Line Control Register
+        return lcr;
+    } else if (offset == 5) {
+        // LSR: Line Status Register
+        return lsr;
+    } else if (offset == 6) {
+        // MSR: Modem Status Register
+        return msr;
+    } else {
+        WARN("uart read offset %d not supported.", offset);
+        return -1;
+    }
+}
+
+bool Uart16650::write(word_t offset, word_t data, int size) {
+    if (size != 1) {
+        WARN("uart write size %d not supported.", size);
+        return false;
+    }
+    if (offset == 0) {
+        if (lcr & 0x80) { // Divisor Latch Access Bit is set
+            // LSB: Lower 8 bits of the divisor latch
+            lsb = data;
+            return true;
+        } else {
+            // THR: Transmitter Holding Register
+            send_byte(data);
+            return true;
+        }
+    } else if (offset == 1) {
+        if (lcr & 0x80) { // Divisor Latch Access Bit is set
+            // MSB: Upper 8 bits of the divisor latch
+            msb = data;
+            return true;
+        } else {
+            // IER: Interrupt Enable Register
+            ier = data;
+            return true;
+        }
+    } else if (offset == 3) {
+        // LCR: Line Control Register
+        lcr = data;
+        return true;
+    } else if (offset == 4) {
+        // Note: MCR is not implemented
+        return true;
+    } else {
+        WARN("uart write offset %d not supported.", offset);
+        return false;
+    }
+}
+
+bool Uart16650::input_data(uint8_t data) {
+    if (queue.size() >= BUFFER_SIZE) {
+        return false;
+    }
+    queue.push(data);
+    return true;
+}
+
+void Uart16650::set_output_stream(std::ostream &os) {
+    if (mode == Mode::SOCKET) {
+        // TODO: close socket and free socket resources
+    } 
+    mode = Mode::STREAM;
+    stream = &os;
+}
+
+void Uart16650::open_socket(const std::string &ip, int port) {
+    if (mode == Mode::STREAM) {
+        stream = nullptr;
+    }
+    mode = Mode::SOCKET;
+    // TODO: open socket
+}
+
+void Uart16650::send_byte(uint8_t c) {
+    if (mode == Mode::STREAM) {
+        if (stream == nullptr) {
+            WARN("uart stream is not set, output to stdout.");
+            std::cout << c;
+        } else {
+            *stream << c;
+        } 
+    } else if (mode == Mode::SOCKET) {
+        // TODO: send data to socket
+    }
+}
+
+uint8_t *Uart16650::get_ptr(word_t offset) {
+    return nullptr;
+}
+
+std::string Uart16650::get_type_str() const {
+    return "uart16650";
+}
