bd19305f11a4e45c39c9445d22703b31b3656f5f
rache <2746324475@qq.com>
2024-12-15 00:21:25 +0800

    fix: remove function memory::read's const tag to support uart

--- src/memory/map.cpp
+++ src/memory/map.cpp
@@ -10,13 +10,15 @@ StorageMemoryMap::~StorageMemoryMap() {
     delete[] data;
 }
 
-word_t StorageMemoryMap::read(word_t offset, int size) const {
+word_t StorageMemoryMap::read(word_t offset, int size) {
     uint64_t ret = 0;
     switch (size) {
         case 1: ret = data[offset]; break;
         case 2: ret = *(uint16_t *)(data + offset); break;
         case 4: ret = *(uint32_t *)(data + offset); break;
+        #ifdef ISA_64
         case 8: ret = *(uint64_t *)(data + offset); break;
+        #endif
         default: break;
     }
     return ret;
@@ -27,7 +29,9 @@ bool StorageMemoryMap::write(word_t offset, word_t data, int size) {
         case 1: *(uint8_t  *)(this->data + offset) = (uint8_t )data; break;
         case 2: *(uint16_t *)(this->data + offset) = (uint16_t)data; break;
         case 4: *(uint32_t *)(this->data + offset) = (uint32_t)data; break;
+        #ifdef ISA_64
         case 8: *(uint64_t *)(this->data + offset) = (uint64_t)data; break;
+        #endif
         default: return false;
     }
     return true;
