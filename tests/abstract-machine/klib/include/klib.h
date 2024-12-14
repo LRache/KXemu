dc3b602914ee60638c7f2caed333550fab03cf0f
rache <2746324475@qq.com>
2024-12-15 00:13:06 +0800

    feat: add klib

---
+++ tests/abstract-machine/klib/include/klib.h
@@ -0,0 +1,31 @@
+#ifndef __KLIB_H__
+#define __KLIB_H__
+
+#include <stddef.h>
+#include <stdarg.h>
+
+// stdio.h
+int printf      (const char *fmt, ...);
+int sprintf     (char *out, const char *fmt, ...);
+int snprintf    (char *out, size_t n, const char *fmt, ...);
+int vsprintf    (char *str, const char *format, va_list ap);
+int vsnprintf   (char *out, size_t n, const char *fmt, va_list ap);
+
+
+// ctype.h
+int isalpha (int c);
+int isdigit (int c);
+
+// string.h
+size_t strlen   (const char *s);
+char  *strcpy   (char *dst, const char *src);
+char  *strncpy  (char *dst, const char *src, size_t n);
+char  *strcat   (char *dst, const char *src);
+int    strcmp   (const char *s1, const char *s2);
+int    strncmp  (const char *s1, const char *s2, size_t n);
+void  *memset   (void *s, int c, size_t n);
+void  *memmove  (void *dst, const void *src, size_t n);
+void  *memcpy   (void *dst, const void *src, size_t n);
+int    memcmp   (const void *s1, const void *s2, size_t n);
+
+#endif
