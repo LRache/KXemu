0aa830f661b48fcbd7e53b2b4df4e34fcee8d6e4
rache <2746324475@qq.com>
2024-12-19 23:24:21 +0800

    feat: use halt pc instead of trap pc

--- src/kdb/cpu-exec.cpp
+++ src/kdb/cpu-exec.cpp
@@ -15,15 +15,15 @@ void kdb::reset_cpu() {
 int static output_and_set_trap(Core *core) {
     int r;
     if (core->is_error()) {
-        std::cout << FMT_FG_RED "Error" FMT_FG_BLUE " at pc=" << FMT_STREAM_WORD(core->get_trap_pc()) << FMT_FG_RESET << std::endl;
+        std::cout << FMT_FG_RED "Error" FMT_FG_BLUE " at pc=" << FMT_STREAM_WORD(core->get_halt_pc()) << FMT_FG_RESET << std::endl;
         r = 1;
         kdb::returnCode = 1;
     } else if (core->is_break()) {
-        r = core->get_trap_code();
+        r = core->get_halt_code();
         if (r == 0) {
-            std::cout << FMT_FG_GREEN "HIT GOOD TRAP" FMT_FG_BLUE " at pc=" << FMT_STREAM_WORD(core->get_trap_pc()) << FMT_FG_RESET << std::endl;
+            std::cout << FMT_FG_GREEN "HIT GOOD TRAP" FMT_FG_BLUE " at pc=" << FMT_STREAM_WORD(core->get_halt_pc()) << FMT_FG_RESET << std::endl;
         } else {
-            std::cout << FMT_FG_RED "HIT BAD TRAP" FMT_FG_BLUE " at pc=" << FMT_STREAM_WORD(core->get_trap_pc()) << FMT_FG_RESET << std::endl;
+            std::cout << FMT_FG_RED "HIT BAD TRAP" FMT_FG_BLUE " at pc=" << FMT_STREAM_WORD(core->get_halt_pc()) << FMT_FG_RESET << std::endl;
         }
         kdb::returnCode = r;
     } else {
