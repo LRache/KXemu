62845e4b50b19c2b1186a7c119754466d2695496
You Zi <2746324475@qq.com>
2025-01-15 15:02:23 +0800

    feat: add menuconfig

---
+++ scripts/config.mk
@@ -0,0 +1,22 @@
+CONFIG_FILE = ./configs/include/config/auto.conf
+CONFIG_DIR = ./configs
+CONF = ./tools/kconfig/build/conf
+MCONF = ./tools/kconfig/build/mconf
+
+ifeq ($(wildcard $(CONFIG_FILE)),)
+$(warning $(CONFIG_FILE) not found, run make menuconfig first)
+endif
+
+-include $(CONFIG_FILE)
+
+$(CONF):
+	@$(MAKE) -C ./tools/kconfig NAME=conf
+
+$(MCONF):
+	@$(MAKE) -C ./tools/kconfig NAME=mconf
+
+menuconfig: $(CONF) $(MCONF)
+	@ cd $(CONFIG_DIR) && ../$(MCONF) -s Kconfig
+	@ cd $(CONFIG_DIR) && ../$(CONF) -s --syncconfig Kconfig
+	@ mkdir -p ./include/generated
+	@ cp $(CONFIG_DIR)/include/generated/autoconf.h ./include
