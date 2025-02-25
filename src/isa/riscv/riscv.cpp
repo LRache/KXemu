09a1804d776097a152c9960ec59cbb42d69c4cf1
You Zi <2746324475@qq.com>
2025-02-25 22:51:49 +0800

    🛠 refactor: move static var into function

--- src/isa/riscv/riscv.cpp
+++ src/isa/riscv/riscv.cpp
@@ -6,18 +6,17 @@
 
 using namespace kxemu;
 
-static const char* gprNames[] = {
-    "zero", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
-    "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
-    "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-    "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
-};
-
 unsigned int isa::get_gpr_count() {
     return 32;
 }
 
 const char *kxemu::isa::get_gpr_name(int idx) {
+    static const char* gprNames[] = {
+        "zero", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+        "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+        "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+        "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+    };
     return gprNames[idx];
 }
 
