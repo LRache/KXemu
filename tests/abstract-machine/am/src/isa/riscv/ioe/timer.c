8842eafbc41da32fc2347506efc4ecdffe27a663
You Zi <2746324475@qq.com>
2025-02-25 22:57:11 +0800

    🛠 refactor: reformat

--- tests/abstract-machine/am/src/isa/riscv/ioe/timer.c
+++ tests/abstract-machine/am/src/isa/riscv/ioe/timer.c
@@ -2,17 +2,17 @@
 #include <stdint.h>
 
 #define MTIME_BASE 0x0200BFF8
-#define MTIME (volatile uint64_t *)MTIME_BASE
-#define MTIME_LOW  (*(volatile uint32_t *)MTIME_BASE)
+#define MTIME_LOW  (*(volatile uint32_t *)(MTIME_BASE + 0))
 #define MTIME_HIGH (*(volatile uint32_t *)(MTIME_BASE + 4))
+#define MTIME MTIME_LOW
 
 uint64_t __am_timer_uptime() {
 #if __riscv_xlen == 32
     uint64_t uptime = MTIME_LOW;
-    uptime |= (uint64_t)MTIME_HIGH << 32;
+    uptime |= ((uint64_t)MTIME_HIGH << 32);
     return uptime / 10;
 #else
-    return *MTIME / 10;
+    return MTIME / 10;
 #endif
 }
 
