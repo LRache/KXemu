3e29f5f0d01fb5536390d88d82d5e746ef443428
Rache <2746324475@qq.com>
2025-02-27 19:58:16 +0800

    fixed

--- src/cpu/riscv/inst/local-decoder.h
+++ src/cpu/riscv/inst/local-decoder.h
@@ -12,5 +12,5 @@
     #define RS1 unsigned int rs1 = decodeInfo.rs1;
     #define RS2 unsigned int rs2 = decodeInfo.rs2;
     #define CSR unsigned int csr = decodeInfo.csr;
-    #define IMM unsigned int csr = decodeInfo.imm;
+    #define IMM unsigned int imm = decodeInfo.imm;
 #endif
