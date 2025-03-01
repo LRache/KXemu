21d49a0294b6530e37d936210cdc2612c3c248ec
You Zi <2746324475@qq.com>
2025-03-01 21:16:30 +0800

    compressed extenstion

--- tests/abstract-machine/scripts/riscv32.mk
+++ tests/abstract-machine/scripts/riscv32.mk
@@ -3,7 +3,7 @@ KXEMU_ISA = riscv32
 
 CROSS_COMPILE ?= riscv64-linux-gnu
 
-COMPILE_FLAGS += -march=rv32im_zicsr -mabi=ilp32
+COMPILE_FLAGS += -march=rv32imac_zicsr -mabi=ilp32
 
 LDFLAGS += -T $(AM_DIR)/scripts/linker.ld \
            --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0 \
