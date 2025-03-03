005eadeeb151875de78a5155a024193059a62c84
You Zi <2746324475@qq.com>
2025-03-03 20:30:06 +0800

    ✨ feat: ioe uart input output test

--- tests/ioe-tests/uart.c
+++ tests/ioe-tests/uart.c
@@ -1,6 +1,12 @@
+#include "am-dev.h"
 #include "klib.h"
 
 int main() {
     printf("Hello, World! This is KXemu!\n");
+    while (1) {
+        uint8_t c = __uart_rx();
+        while (c == 0xff) c = __uart_rx();
+        __am_uart_tx(c);
+    }
     return 0;
 }
