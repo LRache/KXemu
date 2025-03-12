f0864aaebf2ed434bc204089db8e7096ebd5a484
Rache <2746324475@qq.com>
2025-03-12 19:15:34 +0800

    fixed: memory leak of devices

---
+++ src/kdb/device.cpp
@@ -0,0 +1,26 @@
+#include "device/mmio.h"
+#include "kdb/kdb.h"
+#include "device/bus.h"
+
+#include <vector>
+
+using namespace kxemu;
+
+device::Bus *kdb::bus = nullptr;
+
+std::vector<device::MMIODev *> deviceList;
+
+void kdb::device::init() {
+    bus = new kxemu::device::Bus();
+}
+
+void kdb::device::add(kxemu::device::MMIODev *dev) {
+    deviceList.push_back(dev);
+}
+
+void kdb::device::deinit() {
+    for (auto dev : deviceList) {
+        delete dev;
+    }
+    delete bus;
+}
