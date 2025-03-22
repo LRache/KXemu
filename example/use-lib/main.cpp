152f1d4a70d7f2f5720a7109239601443c2a76c8
You Zi <2746324475@qq.com>
2025-03-22 11:43:15 +0800

    fixed: fixed compile error

--- example/use-lib/main.cpp
+++ example/use-lib/main.cpp
@@ -1,6 +1,5 @@
 #include "kxemu/cpu/riscv/cpu.h"
 #include "kxemu/device/bus.h"
-#include "kxemu/device/memory.h"
 #include <iostream>
 #include <cstdint>
 
@@ -8,14 +7,13 @@ using namespace kxemu;
 
 int main() {
     device::Bus bus;
-    device::Memory memory(0x1000);
-    bus.add_memory_map("mem", 0x80000000, 0x1000, &memory);
+    bus.add_memory_map(0x80000000, 0x1000);
     cpu::RVCPU cpu;
     cpu.init(&bus, 0, 1);
     cpu.reset(0x80000000);
     auto core = cpu.get_core(0);
     
-    *(uint32_t *)memory.get_ptr(0) = 0x00100093; // addi x1, x0, 1
+    *(uint32_t *)bus.get_ptr(0x80000000) = 0x00100093; // addi x1, x0, 1
     core->step();
     std::cout << "x1=" << core->get_gpr(1) << std::endl;
 
