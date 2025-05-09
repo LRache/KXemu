ea0c95a831cf117f1e4ad26f3ab28b3f55e5903b
You Zi <2746324475@qq.com>
2025-05-09 18:38:34 +0800

    feat: kdb command load bin

--- src/kdb/cmd/load.cpp
+++ src/kdb/cmd/load.cpp
@@ -1,8 +1,12 @@
 #include "kdb/kdb.h"
 #include "kdb/cmd.h"
+#include "utils/utils.h"
 #include "word.h"
 
+#include <cerrno>
+#include <cstring>
 #include <iostream>
+#include <fstream>
 #include <optional>
 
 using namespace kxemu;
@@ -11,9 +15,11 @@ using namespace kxemu::kdb;
 std::string cmd::elfFileName;
 
 static int cmd_load_elf(const cmd::args_t &);
+static int cmd_load_bin(const cmd::args_t &);
 
 static const cmd::cmd_map_t cmdMap = {
     {"elf", cmd_load_elf},
+    {"bin", cmd_load_bin},
 };
 
 static int cmd_load_elf(const cmd::args_t &args) {
@@ -33,6 +39,34 @@ static int cmd_load_elf(const cmd::args_t &args) {
     kdb::programEntry = entry.value();
     
     return cmd::Success;
+}
+
+static int cmd_load_bin(const cmd::args_t &args) {
+    if (args.size() < 4) {
+        std::cout << "Usage: load bin <filename> <addr>" << std::endl;
+        return cmd::EmptyArgs;
+    }
+    const std::string &filename = args[2];
+    auto addr = utils::string_to_unsigned(args[3]);
+    if (!addr.has_value()) {
+        std::cout << "Invalid address: " << args[3] << std::endl;
+        return cmd::InvalidArgs;
+    }
+
+    std::ifstream f;
+    f.open(filename, std::ios::in | std::ios::binary);
+    if (!f.is_open()) {
+        std::cout << "Open file \"" << filename << "\" FAILED: " << std::strerror(errno) << std::endl;
+        return cmd::CmdError;
+    }
+
+    if (kdb::bus->load_from_stream(f, addr.value())) {
+        std::cout << "Load binary file success." << std::endl;
+        return cmd::Success;
+    } else {
+        std::cerr << "Failed to load binary file " << filename << std::endl;
+        return cmd::CmdError;
+    }
 }
 
 int cmd::load(const cmd::args_t & args) {
