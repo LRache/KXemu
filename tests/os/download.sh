e41a317eb6cf004835d207c53360718304e50d51
You Zi <2746324475@qq.com>
2025-05-29 23:06:19 +0800

    tests: os linux5.15

--- tests/os/download.sh
+++ tests/os/download.sh
@@ -7,7 +7,7 @@ fi
 
 if [ $1 == "linux5.15" ]; then
     wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.132.tar.xz
-    tar -xf linux-5.15.132.tar.xz
+    tar -xvf linux-5.15.132.tar.xz
 else
     echo "Usage: $0 linux5.15"
     exit 1
