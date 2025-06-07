f0945e8299308669decd6874ff5ddab489d4fe28
You Zi <2746324475@qq.com>
2025-06-07 18:03:08 +0800

    fixed: lr.w sign-extended

--- src/cpu/riscv/inst/atomic.cpp
+++ src/cpu/riscv/inst/atomic.cpp
@@ -1,59 +1,71 @@
 #include "cpu/riscv/core.h"
 #include "cpu/word.h"
 #include "device/bus.h"
-#include "log.h"
 
 #include <cstdint>
+#include <expected>
 #include <optional>
 
 #include "./local-decoder.h"
 
 using namespace kxemu::cpu;
-using kxemu::device::AMO;
 
-word_t RVCore::amo_vaddr_translate_and_set_trap(word_t vaddr, int len, bool &valid) {
-    valid = false;
-    
-    if (vaddr & (len - 1)) {
+std::optional<word_t> RVCore::amo_vaddr_translate_and_set_trap(word_t vaddr, int len) {
+    if (unlikely(vaddr & (len - 1))) {
         this->trap(TrapCode::AMO_ACCESS_FAULT, vaddr);
-        return -1;
+        return std::nullopt;
     }
 
     word_t paddr = vaddr;
     if (unlikely(this->privMode != PrivMode::MACHINE)) {
         VMResult vmresult;
         
-        paddr = this->vaddr_translate_core(vaddr, MemType::AMO, vmresult);
-        
-        switch (vmresult) {
-            case VMResult::VM_OK: break;
-            case VMResult::VM_ACCESS_FAULT: this->trap(TrapCode::AMO_ACCESS_FAULT); return -1;
-            case VMResult::VM_PAGE_FAULT:   this->trap(TrapCode::AMO_PAGE_FAULT)  ; return -1;
-            case VMResult::VM_UNSET: PANIC("vmresult is not set.");
+        if (
+            !(this->vaddr_translate_core(vaddr, MemType::AMO)
+            .and_then([&](word_t addr) -> VMResult {
+                paddr = addr;
+                return addr;
+            }).or_else([&](VMFault fault) -> VMResult {
+                switch (fault) {
+                    case VMFault::ACCESS_FAULT: this->trap(TrapCode::AMO_ACCESS_FAULT, vaddr); break;
+                    case VMFault::PAGE_FAULT:   this->trap(TrapCode::AMO_PAGE_FAULT  , vaddr); break;
+                }
+                return std::unexpected(fault);
+            }))
+        ) {
+            return std::nullopt;
         }
 
         bool pmp = this->pmp_check_r(paddr, len) && this->pmp_check_w(paddr, len);
         if (unlikely(!pmp)) {
             this->trap(TrapCode::AMO_ACCESS_FAULT);
-            valid = false;
-            return -1;
+            return std::nullopt;
         }
     }
-    
-    valid = true;
+
     return paddr;
 }
 
-template<typename unit_t>
+template<typename sunit_t>
 void RVCore::do_load_reserved(const DecodeInfo &decodeInfo) {
     TAG_RD; TAG_RS1;
 
-    bool valid;
     word_t addr = SRC1;
-    addr = this->amo_vaddr_translate_and_set_trap(addr, sizeof(unit_t), valid);
-    if (!valid) return;
+    if (
+        !(this->amo_vaddr_translate_and_set_trap(addr, sizeof(sunit_t))
+        .and_then([&](word_t paddr) -> std::optional<word_t> {
+            addr = paddr;
+            return paddr;
+        }).or_else([&]() -> std::optional<word_t> {
+            this->trap(TrapCode::AMO_ACCESS_FAULT, addr);
+            return std::nullopt;
+        })
+    )) {
+        return;
+    }
 
-    unit_t value = this->bus->read(addr, sizeof(unit_t), valid);
+    bool valid;
+    sunit_t value = (sword_t)(sunit_t)this->bus->read(addr, sizeof(sunit_t), valid);
     if (!valid) {
         this->trap(TrapCode::AMO_ACCESS_FAULT, addr);
         return;
@@ -61,7 +73,6 @@ void RVCore::do_load_reserved(const DecodeInfo &decodeInfo) {
 
     DEST = value;
     this->reservedMemory[addr] = value;
-    // INFO("Load reserved at address=" FMT_WORD ", pc=" FMT_WORD ", value=" FMT_WORD, addr, this->pc, (word_t)value);
 }
 
 template<typename unit_t>
@@ -69,9 +80,19 @@ void RVCore::do_store_conditional(const DecodeInfo &decodeInfo) {
     TAG_RD; TAG_RS1; TAG_RS2;
 
     bool valid;
-    word_t addr = SRC1;
-    addr = this->amo_vaddr_translate_and_set_trap(addr, sizeof(unit_t), valid);
-    if (!valid) return;
+    word_t addr;
+    if (
+        !(this->amo_vaddr_translate_and_set_trap(SRC1, sizeof(unit_t))
+        .and_then([&](word_t paddr) -> std::optional<word_t> {
+            addr = paddr;
+            return paddr;
+        }).or_else([&]() -> std::optional<word_t> {
+            this->trap(TrapCode::AMO_ACCESS_FAULT, addr);
+            return std::nullopt;
+        })
+    )) {
+        return;
+    }
 
     auto iter = this->reservedMemory.find(addr);
     if (iter == this->reservedMemory.end()) {
@@ -90,38 +111,30 @@ void RVCore::do_store_conditional(const DecodeInfo &decodeInfo) {
     bool success = __atomic_compare_exchange_n(ptr, &expected, desired, false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
     DEST = success ? 0 : 1;
     this->reservedMemory.erase(iter);
-
-    // if (!success) {
-    //     // Store conditional failed
-    //     INFO("Store conditional failed at address=" FMT_WORD ", pc=" FMT_WORD ", expected=" FMT_WORD ", v=" FMT_WORD, addr, this->pc, iter->second, *(word_t *)ptr);
-    //     return;
-    // }
 }
 
-template<kxemu::device::AMO amo, typename sw_t>
+template<kxemu::device::AMO amo, typename sunit_t>
 void RVCore::do_amo_inst(const DecodeInfo &decodeInfo) {
     TAG_RD; TAG_RS1; TAG_RS2
     
-    constexpr int len = sizeof(sw_t);
+    constexpr int LEN = sizeof(sunit_t);
 
-    bool valid;
-    word_t paddr = this->amo_vaddr_translate_and_set_trap(SRC1, len, valid);
-    if (!valid) return;
+    word_t paddr;
+    if (
+        !(this->amo_vaddr_translate_and_set_trap(SRC1, LEN)
+        .and_then([&](word_t addr) -> std::optional<word_t> {
+            paddr = addr;
+            return addr;
+        }).or_else([&]() -> std::optional<word_t> {
+            this->trap(TrapCode::AMO_ACCESS_FAULT, SRC1);
+            return std::nullopt;
+        })
+    )) {
+        return;
+    }
 
-    // word_t src = SRC2;
-    // sw_t oldValue = this->bus->do_atomic(addr, src, len, amo, valid); // signed extend
-    
-    // if (!valid) {
-    //     this->trap(TrapCode::AMO_ACCESS_FAULT, addr);
-    //     return;
-    // }
-
-    // DEST = (sword_t)oldValue;
-    this->bus->do_atomic(paddr, SRC2, len, amo).and_then([&](word_t oldValue) -> std::optional<word_t> {
-        if (paddr == 0x81858780) {
-            INFO("AMO at 0x81858780, amo=%d, oldValue=" FMT_WORD ", data=" FMT_WORD ", pc=" FMT_WORD, (int)amo, oldValue, SRC2, this->pc);
-        }
-        DEST = (sw_t)oldValue;
+    this->bus->do_atomic(paddr, SRC2, LEN, amo).and_then([&](word_t oldValue) -> std::optional<word_t> {
+        DEST = (sword_t)(sunit_t)oldValue;
         return oldValue;
     }).or_else([&]() -> std::optional<word_t> {
         this->trap(TrapCode::AMO_ACCESS_FAULT, paddr);
@@ -130,101 +143,93 @@ void RVCore::do_amo_inst(const DecodeInfo &decodeInfo) {
 }
 
 void RVCore::do_lr_w(const DecodeInfo &decodeInfo) {
-   this->do_load_reserved<uint32_t>(decodeInfo);
+   this->do_load_reserved<int32_t>(decodeInfo);
 }
 
 void RVCore::do_sc_w(const DecodeInfo &decodeInfo) {
-    this->do_store_conditional<uint32_t>(decodeInfo);
+    this->do_store_conditional<int32_t>(decodeInfo);
 }
 
 void RVCore::do_amoswap_w(const DecodeInfo &decodeInfo) {
-    this->do_amo_inst<AMO::SWAP, int32_t>(decodeInfo);
+    this->do_amo_inst<device::AMO::SWAP, int32_t>(decodeInfo);
 }
 
 void RVCore::do_amoadd_w(const DecodeInfo &decodeInfo) {
-    this->do_amo_inst<AMO::ADD, int32_t>(decodeInfo);
+    this->do_amo_inst<device::AMO::ADD, int32_t>(decodeInfo);
 }
 
 void RVCore::do_amoxor_w(const DecodeInfo &decodeInfo) {
-    this->do_amo_inst<AMO::XOR, int32_t>(decodeInfo);
+    this->do_amo_inst<device::AMO::XOR, int32_t>(decodeInfo);
 }
 
 void RVCore::do_amoand_w(const DecodeInfo &decodeInfo) {
-    this->do_amo_inst<AMO::AND, int32_t>(decodeInfo);
+    this->do_amo_inst<device::AMO::AND, int32_t>(decodeInfo);
 }
 
 void RVCore::do_amoor_w(const DecodeInfo &decodeInfo) {
-    this->do_amo_inst<AMO::OR, int32_t>(decodeInfo);
+    this->do_amo_inst<device::AMO::OR, int32_t>(decodeInfo);
 }
 
 void RVCore::do_amomin_w(const DecodeInfo &decodeInfo) {
-    // this->do_amo_inst<AMO::MIN, int32_t>(decodeInfo);
-    this->do_invalid_inst();
+    this->do_amo_inst<device::AMO::MIN, int32_t>(decodeInfo);
 }
 
 void RVCore::do_amomax_w(const DecodeInfo &decodeInfo) {
-    // this->do_amo_inst<AMO::MAX, int32_t>(decodeInfo);
-    this->do_invalid_inst();
+    this->do_amo_inst<device::AMO::MAX, int32_t>(decodeInfo);
 }
 
 void RVCore::do_amominu_w(const DecodeInfo &decodeInfo) {
-    // this->do_amo_inst<AMO::MINU, int32_t>(decodeInfo);
-    this->do_invalid_inst();
+    this->do_amo_inst<device::AMO::MINU, int32_t>(decodeInfo);
 }
 
 void RVCore::do_amomaxu_w(const DecodeInfo &decodeInfo) {
-    // this->do_amo_inst<AMO::MAXU, int32_t>(decodeInfo);
-    this->do_invalid_inst();
+    this->do_amo_inst<device::AMO::MAXU, int32_t>(decodeInfo);
 }
 
 #ifdef KXEMU_ISA64
 
 void RVCore::do_lr_d(const DecodeInfo &decodeInfo) {
-    this->do_load_reserved<uint64_t>(decodeInfo);
+    this->do_load_reserved<int64_t>(decodeInfo);
 }
 
 void RVCore::do_sc_d(const DecodeInfo &decodeInfo) {
-    this->do_store_conditional<uint64_t>(decodeInfo);
+    this->do_store_conditional<int64_t>(decodeInfo);
 }
 
 void RVCore::do_amoswap_d(const DecodeInfo &decodeInfo) {
-    this->do_amo_inst<AMO::SWAP, int64_t>(decodeInfo);
+    this->do_amo_inst<device::AMO::SWAP, int64_t>(decodeInfo);
 }
 
 void RVCore::do_amoadd_d(const DecodeInfo &decodeInfo) {
-    this->do_amo_inst<AMO::ADD, int64_t>(decodeInfo);
+    this->do_amo_inst<device::AMO::ADD, int64_t>(decodeInfo);
 }
 
 void RVCore::do_amoxor_d(const DecodeInfo &decodeInfo) {
-    this->do_amo_inst<AMO::XOR, int64_t>(decodeInfo);
+    this->do_amo_inst<device::AMO::XOR, int64_t>(decodeInfo);
 }
 
 void RVCore::do_amoand_d(const DecodeInfo &decodeInfo) {
-    this->do_amo_inst<AMO::AND, int64_t>(decodeInfo);
+    this->do_amo_inst<device::AMO::AND, int64_t>(decodeInfo);
 }
 
 void RVCore::do_amoor_d(const DecodeInfo &decodeInfo) {
-    this->do_amo_inst<AMO::OR, int64_t>(decodeInfo);
+    this->do_amo_inst<device::AMO::OR, int64_t>(decodeInfo);
 }
 
 void RVCore::do_amomin_d(const DecodeInfo &decodeInfo) {
-    // this->do_amo_inst<AMO::AMO_MIN, int64_t>(decodeInfo);
-    this->do_invalid_inst();
+    this->do_amo_inst<device::AMO::MIN, int64_t>(decodeInfo);
 }
 
 void RVCore::do_amomax_d(const DecodeInfo &decodeInfo) {
-    // this->do_amo_inst<AMO::AMO_MAX, int64_t>(decodeInfo);
-    this->do_invalid_inst();
+    this->do_amo_inst<device::AMO::MAX, int64_t>(decodeInfo);
 }
 
 void RVCore::do_amominu_d(const DecodeInfo &decodeInfo) {
-    // this->do_amo_inst<AMO::AMO_MINU, int64_t>(decodeInfo);
-    this->do_invalid_inst();
+    this->do_amo_inst<device::AMO::MINU, int64_t>(decodeInfo);
 }
 
 void RVCore::do_amomaxu_d(const DecodeInfo &decodeInfo) {
-    // this->do_amo_inst<AMO::AMO_MAXU, int64_t>(decodeInfo);
-    this->do_invalid_inst();
+    this->do_amo_inst<device::AMO::MAXU, int64_t>(decodeInfo);
 }
 
 #endif
